ce6324a4b0bb92fe831b28f6f1dfc4f656e195ba
Robert MURDOCH <rmurdoch@c1r1s3.wethinkcode.co.za>
2018-08-03 11:55:50 +0200

    changed filestructure

--- test/main.cpp
+++ test/main.cpp
@@ -24,15 +24,9 @@ typedef std::vector<std::string> strings;
 //     return;
 // }
 std::string removeComment(std::string s){
- // std::cout << "test0";
-    // std::string s = "; -------------\n; exemple.avm -\n; -------------\n\npush int32(42)\npush int32(33)\n\nadd\n\npush float(44.55)\n\nmul\n\npush double(42.42)\npush int32(42)\n\ndump\n\npop\n\nassert double(42.42)\n\nexit\n";
-    // std::string s = "mon;day tuesday wednesday";
-    // std::cout << s;
     std::istringstream s1(s);
-    // std::cout << "test2";
     char str[s.length()];
     s1.get(str, s.length(), ';');     // reads "llo,"
-    // std::cout << str << std::endl;
     return (str);
 }
 
